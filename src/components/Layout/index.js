e58d18b38d2f50b16bd01ab8a01483cb9374e445
Rayan Ballout <rayanballout14@gmail.com>
2022-02-04 21:12:51 +0200

    Structuring

---
+++ src/components/Layout/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+// style
+import './Layout.css'
+
+// components
+import Header from '../Header';
+import Footer from '../Footer';
+
+function Layout({ children }) {
+    return (
+        <>
+            <Header />
+            <main>
+                {children}
+            </main>
+            <Footer />
+        </>
+
+    );
+}
+
+export default Layout;
